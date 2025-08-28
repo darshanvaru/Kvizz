5a895c97f75841fd275a287d38cc4b3a28b0f6a3
DarshanVaru <darshanvaru2003@gmail.com>
2025-08-28 15:54:38 +0530

    Refactored code and solved issues needed

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -7,53 +7,8 @@ class UserProvider with ChangeNotifier {
 
   UserModel? get currentUser => _currentUser;
 
-  void setUser(UserModel user) {
+  void setCurrentUser(UserModel user) {
     _currentUser = user;
     notifyListeners();
-  }
-
-  // Add methods for new fields
-  void updateUserStats(UserStats stats) {
-    if (_currentUser != null) {
-      _currentUser = UserModel(
-        id: _currentUser!.id,
-        name: _currentUser!.name,
-        email: _currentUser!.email,
-        mobile: _currentUser!.mobile,
-        username: _currentUser!.username,
-        photo: _currentUser!.photo,
-        createdAt: _currentUser!.createdAt,
-        passwordChangedAt: _currentUser!.passwordChangedAt,
-        active: _currentUser!.active,
-        deletedAt: _currentUser!.deletedAt,
-        stats: stats, // Updated stats
-        ownedQuizzes: _currentUser!.ownedQuizzes,
-        playedQuiz: _currentUser!.playedQuiz,
-        settings: _currentUser!.settings,
-      );
-      notifyListeners();
-    }
-  }
-
-  void updateUserSettings(UserSettings settings) {
-    if (_currentUser != null) {
-      _currentUser = UserModel(
-        id: _currentUser!.id,
-        name: _currentUser!.name,
-        email: _currentUser!.email,
-        mobile: _currentUser!.mobile,
-        username: _currentUser!.username,
-        photo: _currentUser!.photo,
-        createdAt: _currentUser!.createdAt,
-        passwordChangedAt: _currentUser!.passwordChangedAt,
-        active: _currentUser!.active,
-        deletedAt: _currentUser!.deletedAt,
-        stats: _currentUser!.stats,
-        ownedQuizzes: _currentUser!.ownedQuizzes,
-        playedQuiz: _currentUser!.playedQuiz,
-        settings: settings, // Updated settings
-      );
-      notifyListeners();
-    }
   }
 }
